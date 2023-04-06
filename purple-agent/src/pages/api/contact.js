33a2a11bd3c82b5f45ed5f544992d6440f2f3c8f
Deniz <denizhincal05@gmail.com>
2023-04-07 00:36:24 +0300

    Design of Email Content

--- purple-agent/src/pages/api/contact.js
+++ purple-agent/src/pages/api/contact.js
@@ -15,8 +15,9 @@ const handler = async (req, res) => {
         ...mailOptions,
 
         subject: data.name,
-        text: "this is a text",
-        html: "<h1>Test heading</h1><p>some body text</p>"
+        text: data.name + "" + data.email + "" + data.phone,
+        text: data.message,
+        html: `<h1>İsim: ${data.name}</h1> <h1>Email: ${data.email}</h1> <h1>Telefon: ${data.phone}</h1> <p>${data.message}</p>`
       })
 
       return res.status(200).json({ "message": "Success" })
