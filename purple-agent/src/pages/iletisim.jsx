896f1e79c1696c41203318dd34803125e17634fe
Deniz <denizhincal05@gmail.com>
2023-04-24 15:07:17 +0300

    Change Favicon of Website

--- purple-agent/src/pages/iletisim.jsx
+++ purple-agent/src/pages/iletisim.jsx
@@ -57,9 +57,9 @@ export default function ContactUs() {
     return (
         <>
             <Head>
-                <title>Purple Agent - Bize Ulaşın</title>
+                <title>Purple Agent - İletişim</title>
                 <meta name="description" content="Purple Agent" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="shortcut icon" href="/icon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
--- purple-agent/src/pages/kimiz.jsx
+++ purple-agent/src/pages/kimiz.jsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Head>
             <title>Purple Ajans - Kimiz Biz</title>
             <meta name="description" content="Purple Agent" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="shortcut icon" href="/icon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
