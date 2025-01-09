93e06f87834778fe78c5a5748c5107cd32bc1b81
Deniz Hınçal <dhinc@uic.edu>
2025-01-08 22:58:04 -0600

    Update Babel Config

--- purple-agent/src/pages/api/contact.js
+++ purple-agent/src/pages/api/contact.js
@@ -3,11 +3,10 @@
 import { mailOptions, sender } from "@/config/nodemailer";
 
 const handler = async (req, res) => {
-
   if (req.method === "POST") {
     const data = req.body;
     if (!data.name || !data.email || !data.message || !data.phone) {
-      return res.status(400).json({ "message": 'Bad Request' })
+      return res.status(400).json({ message: "Bad Request" });
     }
 
     try {
@@ -17,19 +16,17 @@ const handler = async (req, res) => {
         subject: data.name,
         text: data.name + "" + data.email + "" + data.phone,
         text: data.message,
-        html: `<h1>İsim: ${data.name}</h1> <h1>Email: ${data.email}</h1> <h1>Telefon: ${data.phone}</h1> <p>${data.message}</p>`
-      })
+        html: `<h1>İsim: ${data.name}</h1> <h1>Email: ${data.email}</h1> <h1>Telefon: ${data.phone}</h1> <p>${data.message}</p>`,
+      });
 
-      return res.status(200).json({ "message": "Success" })
-
+      return res.status(200).json({ message: "Success" });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({ "message": err.message })
+      return res.status(400).json({ message: err.message });
     }
   }
 
-
-  return res.status(400).json({ "message": 'Bad Request' })
-}
+  return res.status(400).json({ message: "Bad Request" });
+};
 
 export default handler;
